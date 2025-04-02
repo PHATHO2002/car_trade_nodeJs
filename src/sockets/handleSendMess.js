3f38d75d838b5641e54eb408850ce810b6453321
phat ho <phatho1508@gmail.com>
2025-04-02 11:40:32 +0700

    02/04/2025

--- src/sockets/handleSendMess.js
+++ src/sockets/handleSendMess.js
@@ -1,16 +1,20 @@
 const sendMessToSocket = (io, socket, usersOnline) => {
     socket.on('send_message', (data) => {
         const { senderId, receiverId, message, username } = data; // `to` là username của người nhận
-        const userIndex = usersOnline.findIndex((user) => user.userId === receiverId);
 
-        // Kiểm tra xem client có tồn tại không và gửi tin nhắn
-        if (userIndex >= 0) {
-            io.to(usersOnline[userIndex].socketId).emit('receive_message', data); // Gửi tin nhắn đến client cụ thể
+        // Kiểm tra xem người nhận có online hay không
+        const receiver = usersOnline[receiverId];
+
+        if (receiver) {
+            // Nếu người nhận online, gửi tin nhắn đến socketId của người nhận
+            io.to(receiver).emit('receive_message', data); // Gửi tin nhắn đến client cụ thể
         } else {
+            // Nếu người nhận không online, gửi lỗi về client
             socket.emit('send_message_error', {
                 error: `User with ID ${receiverId} is not online.`, // Lỗi gửi tin nhắn
             });
         }
     });
 };
+
 module.exports = sendMessToSocket;
